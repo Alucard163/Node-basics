6d414fec49a3762fb1d5c65f936e2110b5baa395
Alucard163 <romemix163@gmail.com>
2021-01-24 12:23:19 +0400

    Init

---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require ('os')
+
+//Platform
+console.log(os.platform())
+
+//Arch
+console.log(os.arch())
+
+//Info
+console.log(os.cpus())
+
+//Free memory
+console.log(os.freemem())
+
+// Total memory
+console.log(os.totalmem())
+
+//Home directory
+console.log(os.homedir())
+
+//Work time
+console.log(os.uptime())
